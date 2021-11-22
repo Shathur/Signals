5fa79bfc4eb9c930a93e2a240f2782eb38891469
unknown <80460125+Shathur@users.noreply.github.com>
2021-11-22 12:26:15 +0200

    added comments

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 def start_end_date(df):
+    """
+
+    :param df: dataframe that must contain one column named 'date' in the datetime format
+    :return: start date and end date as strings
+    """
     start_date = str(df['date'].iloc[0].date().month) + '-' + str(df['date'].iloc[0].date().day) + '-' + str(df['date'].iloc[0].date().year)
     end_date = str(df['date'].iloc[-1].date().month) + '-' + str(df['date'].iloc[-1].date().day) + '-' + str(df['date'].iloc[-1].date().year)
     return start_date, end_date
