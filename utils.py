b7f80d152e0f81785229fb20fbb3f6e0ca380e23
unknown <80460125+Shathur@users.noreply.github.com>
2021-11-22 12:36:08 +0200

    added weeks_between_dates

--- utils.py
+++ utils.py
@@ -1,9 +1,29 @@
+from datetime import timedelta
+
+
 def start_end_date(df):
     """
 
     :param df: dataframe that must contain one column named 'date' in the datetime format
     :return: start date and end date as strings
     """
-    start_date = str(df['date'].iloc[0].date().month) + '-' + str(df['date'].iloc[0].date().day) + '-' + str(df['date'].iloc[0].date().year)
-    end_date = str(df['date'].iloc[-1].date().month) + '-' + str(df['date'].iloc[-1].date().day) + '-' + str(df['date'].iloc[-1].date().year)
+    start_date = str(df['date'].iloc[0].date().month) + '-' + str(df['date'].iloc[0].date().day) + '-' + str(
+        df['date'].iloc[0].date().year)
+    end_date = str(df['date'].iloc[-1].date().month) + '-' + str(df['date'].iloc[-1].date().day) + '-' + str(
+        df['date'].iloc[-1].date().year)
     return start_date, end_date
+
+
+def weeks_between_dates(d1, d2):
+    """
+
+    :param d1: first date in datetime format
+    :param d2: second date in datetime format
+    :return: weeks
+    """
+    monday1 = (d1 - timedelta(days=d1.weekday()))
+    monday2 = (d2 - timedelta(days=d2.weekday()))
+
+    weeks = (monday2 - monday1).days // 7
+
+    return weeks
