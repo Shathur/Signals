cd9730216d523a89c4ac8c267981e08472491487
unknown <80460125+Shathur@users.noreply.github.com>
2021-11-22 13:28:49 +0200

    new train.py and added score, run_analytics, plot_fi

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 from datetime import timedelta
+import matplotlib.pyplot as plt
 
 
 def start_end_date(df):
@@ -27,3 +29,44 @@ def weeks_between_dates(d1, d2):
     weeks = (monday2 - monday1).days // 7
 
     return weeks
+
+
+def plot_feature_importances(feature_names, model):
+    plt.figure(figsize=(15, 3))
+    plt.bar(feature_names, model.feature_importances_)
+    plt.xticks(rotation=70)
+    plt.show()
+
+
+# From Jason Rosenfeld's notebook
+# https://twitter.com/jrosenfeld13/status/1315749231387443202?s=20
+
+def score(df, target_name, pred_name):
+    """
+    Takes df and calculates spearm correlation from pre-defined cols
+    """
+    # method="first" breaks ties based on order in array
+    return np.corrcoef(
+        df[target_name],
+        df[[pred_name]].rank(pct=True, method="first")
+    )[0, 1]
+
+
+def run_analytics(era_scores):
+    print(f"Mean Correlation: {era_scores.mean():.4f}")
+    print(f"Median Correlation: {era_scores.median():.4f}")
+    print(f"Standard Deviation: {era_scores.std():.4f}")
+    print('\n')
+    print(f"Mean Pseudo-Sharpe: {era_scores.mean() / era_scores.std():.4f}")
+    print(f"Median Pseudo-Sharpe: {era_scores.median() / era_scores.std():.4f}")
+    print('\n')
+    hit_rate = era_scores.apply(lambda x: np.sign(x)).value_counts()[1] / len(era_scores)
+    print(f'Hit Rate (% positive eras): {hit_rate:.2%}')
+
+    # era_scores.rolling(10).mean().plot(kind='line', title='Rolling Per Era Correlation Mean', figsize=(15,4))
+    # plt.axhline(y=0.0, color="r", linestyle="--"); plt.show()
+
+    # era_scores.cumsum().plot(title='Cumulative Sum of Era Scores', figsize=(15,4))
+    # plt.axhline(y=0.0, color="r", linestyle="--"); plt.show()
+
+    return hit_rate
