352aef7a0bcf1bbf99946c01164ac8558f029b67
unknown <80460125+Shathur@users.noreply.github.com>
2021-12-27 12:00:19 +0200

    added custom params flexibility in run and create_models

--- models.py
+++ models.py
@@ -2,11 +2,12 @@ from xgboost import XGBRegressor
 import lightgbm as lgb
 
 
-def run_model(train_data=None, val_data=None, model_type='xgb', save_to_drive=False, save_folder=None, cv_count=None):
+def run_model(train_data=None, val_data=None, model_type='xgb', model_params=None, save_to_drive=False,
+              save_folder=None, cv_count=None):
     X_train, y_train = train_data
     X_val, y_val = val_data
 
-    model = create_model(model_type=model_type)
+    model = create_model(model_type=model_type, model_params=model_params)
 
     model.fit(X_train, y_train, eval_set=[(X_val, y_val)], early_stopping_rounds=10, verbose=False)
 
@@ -18,7 +19,21 @@ def run_model(train_data=None, val_data=None, model_type='xgb', save_to_drive=Fa
     return model
 
 
-def create_model(model_type='xgb'):
+def create_model(model_type='xgb', model_params=None):
+    if model_params is None:
+        model_params = get_default_params(model_type=model_type)
+    else:
+        pass
+    if model_type == 'lgb':
+        model = lgb.LGBMRegressor(params=model_params)
+
+    if model_type == 'xgb':
+        model = XGBRegressor(params=model_params)
+
+    return model
+
+
+def get_default_params(model_type='xgb'):
     if model_type == 'lgb':
         params = {
             'learning_rate': 0.01,
@@ -28,10 +43,15 @@ def create_model(model_type='xgb'):
             'colsample_bytree': 0.6,
             'device': "gpu",
         }
-        model = lgb.LGBMRegressor(params=params)
+    elif model_type == 'xgb':
+        params = {
+            'max_depth' : 5,
+            'learning_rate' : 0.01,
+            'n_estimators' : 1000,
+            'n_jobs' : -1,
+            'colsample_bytree' : 0.6,
+            'tree_method' : 'gpu_hist',
+            'verbosity' : 0
+        }
 
-    if model_type == 'xgb':
-        model = XGBRegressor(max_depth=4, learning_rate=0.01, n_estimators=100, n_jobs=-1, colsample_bytree=0.1,
-                             tree_method='gpu_hist', verbosity=0)  # tree_method='gpu_hist',
-
-    return model
+    return params
