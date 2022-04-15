1fd1421ea6eaa4755616a50ed6638d60bbff77b4
Shathur <iliaslangas@gmail.com>
2022-04-15 13:00:50 +0300

    added train_CV

--- train.py
+++ train.py
@@ -111,3 +111,90 @@ def train_val(df, feature_names, target_name, pred_name, cv_split_data, date_col
                    tour_preds_total]
 
     return feat_importances_df, diagnostics_per_split_df, preds_total
+
+def train_CV(data_dir, last_friday, features_boundaries, model_name, target_name=TARGET_NAME, pred_name=PREDICTION_NAME,
+             n_splits=4,
+             submit=True, submit_diagnostics=False, submit_reverse=False, submit_diagnostics_reverse=False,
+             model_name_reverse=None,
+             models_save_folder='/content/mymodels/', upload_name='signal_upload'):
+    # # preprocess data
+    # prices_df, train_df, feature_names = prepare_dataset(data_dir, features_start)
+
+    train_df = pd.read_parquet(data_dir)
+
+    train_without_live = train_df[train_df['friday_date'] < last_friday]
+    train_without_live.dropna(inplace=True)
+    train_df = pd.concat([train_without_live, train_df[train_df['friday_date'] == last_friday]])
+
+    feature_names = train_df.columns[
+                    train_df.columns.str.find(features_boundaries[0]).argmax(): train_df.columns.str.find(
+                        features_boundaries[1]).argmax() + 1].tolist()
+
+    # split data
+    cv_split_data = cv_split_creator(df=train_df, col='friday_date', n_splits=n_splits)
+
+    # keep data for validation of the CV strategy
+    tour_data = train_df.iloc[cv_split_data[0][1]]
+
+    if os.path.isdir(models_save_folder):
+        shutil.rmtree(models_save_folder)  # delete dir + models
+    os.mkdir(models_save_folder)  # create dir from scratch
+
+    metrics = train_val(df=train_df,
+                        date_col='friday_date',
+                        feature_names=feature_names,
+                        target_name=TARGET_NAME,
+                        pred_name=PREDICTION_NAME,
+                        cv_split_data=cv_split_data,
+                        tour_df=tour_data,
+
+                        save_to_drive=True,
+                        save_folder=models_save_folder,
+                        visualize=True)
+
+    # preds_total = metrics[2]
+    # tour_preds_total = preds_total[2]
+
+    # # ensemble the predictions
+    # tour_data[PREDICTION_NAME] = np.mean(tour_preds_total, axis=0)
+    # # calculate scores
+    # tour_era_scores = tour_data.groupby(tour_data['date']).apply(lambda x: score(x, target_name, pred_name))
+    # # for validation metrics
+    # # run_analytics(tour_era_scores)
+
+    validation_predictions = np.mean(metrics[2][2], axis=0)
+
+    validation_sub = tour_data.copy()
+
+    validation_sub['signal'] = validation_predictions
+
+    last_friday = datetime.now() + relativedelta(weekday=FR(-1))
+    last_friday = int(last_friday.strftime('%Y%m%d'))
+
+    # live sub
+    train_df.loc[train_df['friday_date'] == last_friday, 'data_type'] = 'live'
+
+    live_sub = train_df.query('data_type == "live"').copy()
+
+    live_sub['signal'] = get_predictions(df=live_sub[feature_names],
+                                         num_models=4,
+                                         folder_name='/content/mymodels/')
+
+    # concat valid and test
+    sub = pd.concat([validation_sub, live_sub], ignore_index=True)
+
+    # select necessary columns
+    sub = sub[['ticker', 'friday_date', 'data_type', 'signal']]
+
+    # submit
+    submit_signal(sub, public_id, secret_key, submit, submit_diagnostics, model_name, upload_name=upload_name)
+
+    # submit reverse
+    reverse_sub = sub
+    reverse_sub['signal'] = reverse_sub.groupby('friday_date')['signal'].rank(pct=True, method="first", ascending=False)
+    reverse_sub.reset_index(drop=True, inplace=True)
+    submit_signal(reverse_sub, public_id, secret_key, submit_reverse, submit_diagnostics_reverse, model_name_reverse,
+                  upload_name)
+
+    # free memory
+    gc.collect()
