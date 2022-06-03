0953842f716f0f318c99367d4f145809d1c49535
Shathur <iliaslangas@gmail.com>
2022-06-03 16:31:38 +0300

    added correct get_model_lst

--- model_handling.py
+++ model_handling.py
@@ -2,14 +2,29 @@ from xgboost import XGBRegressor, XGBClassifier
 import lightgbm as lgb
 
 
-# get models into a  list for iteration on them
-def get_model_lst(num_models=1, folder_name=None):
-    model_lst = []
-    for cv_num in range(num_models):
-        model_lst.append(folder_name + 'model_{}.xgb'.format(cv_num))
-    print(model_lst)
+# get models into a list for iteration on them
+def get_model_lst(num_models=1, prefix=None, folder_name=None):
+    """
+    Parameters
+    ----------
+    num_models: If 0 keep all the models in the folder
+    prefix
+    folder_name
+    Returns
+    -------
+    """
+    model_lst = [folder_name + x for x in os.listdir(folder_name)]
+    if prefix is not None:
+        model_lst = [x for x in model_lst if x.startswith(folder_name+prefix)]
+    else:
+        pass
+    if num_models != 0:
+        model_lst_final = model_lst[0:num_models]
+    else:
+        model_lst_final = model_lst
+    print(model_lst_final)
 
-    return model_lst
+    return model_lst_final
 
 
 def run_model(train_data=None, val_data=None, model_type='xgb', task_type='regression', model_params=None,
