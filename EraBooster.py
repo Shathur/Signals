2bfc17b80959f2cd1a5c79dca0124b2bf817fb28
Shathur <iliaslangas@gmail.com>
2023-03-29 14:06:28 +0300

    commit from oracle

--- EraBooster.py
+++ EraBooster.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from Signals.predictions import get_predictions_per_era
+from Signals.predictions import predict_in_era_batch
 from Signals.utils import get_era_idx, spearman
 
 class EraBooster:
@@ -42,6 +42,10 @@ class EraBooster:
         self.quantile = quantile
         
         self.iteration = 0
+        self.validation_prior = 0
+        self.sharpe_prior = 0
+        self.keep_sharpe_lst = []
+        self.era_scores_test_prior = 0
         self.era_diff_test_total_lst = []
         self.worst_eras_total = []
 
@@ -60,40 +64,51 @@ class EraBooster:
         self.model.fit(
             train_data['X'],
             train_data['y'],
-            eval_set=test_data,
+            eval_set=[(test_data['X'],test_data['y'])],
     )
 
-    def train_once(self, iter_count):
-        print(f"Adding {added_trees} trees and training on worst eras ... Iter: {iter_count}")
-        self.model.n_estimators += trees_per_step
+    def train_once(
+        self,
+        iter_count,
+        train_data,
+        test_data
+    ):
+        print(f"Adding {self.added_trees} trees and training on worst eras ... Iter: {iter_count}")
+        self.model.n_estimators += self.added_trees
         booster = self.model.get_booster()
         self.model.fit(
+            train_data['X'],
+            train_data['y'],
+            eval_set=[(test_data['X'],test_data['y'])],
             
     )
 
-    def return_predictions(self, train_df, test_df, validation_df):
+    def return_predictions(self, train_df, test_df, validation_df, feature_cols):
         """
         return the predictions as a list of np.arrays for 
         train, test and validation sets
         """
         train_indices = get_era_idx(train_df,self.date_col)
-        preds_train = get_predictions_per_era(
-            df=train_df,
-            num_models=1,
+        preds_train = predict_in_era_batch(
+            model=self.model,
+            df=train_df[feature_cols],
             era_idx=train_indices,
-        )
+            rank_per_era=True,
+        ) 
         test_indices = get_era_idx(test_df,self.date_col)
-        preds_test = get_predictions_per_era(
-            df=test_df,
-            num_models=1,
+        preds_test = predict_in_era_batch(
+            model=self.model,
+            df=test_df[feature_cols],
             era_idx=test_indices,
-        )
+            rank_per_era=True,
+        ) 
         validation_indices = get_era_idx(validation_df,self.date_col)
-        preds_validation = get_predictions_per_era(
-            df=validation_df,
-            num_models=1,
+        preds_validation = predict_in_era_batch(
+            model=self.model,
+            df=validation_df[feature_cols],
             era_idx=validation_indices,
-        )
+            rank_per_era=True,
+        ) 
         return preds_train, preds_test, preds_validation
 
     def return_era_scores(self, train_df, test_df):
@@ -104,14 +119,14 @@ class EraBooster:
         era_scores_train = pd.Series(index=train_df[self.date_col].unique())
         for era in era_scores_train.index:
             era_df = train_df[train_df[self.date_col] == era]
-            era_scores_train[era] = spearman(era_df[self.prediction_col], era_df[self.target_col]).correlation
+            era_scores_train[era] = spearman(era_df[self.prediction_col], era_df[self.target_col]) # .correlation
         era_scores_test = pd.Series(index=test_df[self.date_col].unique())
         for era in era_scores_test.index:
             era_df = test_df[test_df[self.date_col] == era]
-            era_scores_test[era] = spearman(era_df[self.prediction_col], era_df[self.target_col]).correlation
+            era_scores_test[era] = spearman(era_df[self.prediction_col], era_df[self.target_col]) # .correlation
         return era_scores_train, era_scores_test
 
-    def era_diff(self, test_df, era_scores_test, era_scores_test_prior):
+    def era_diff(self, test_df, era_scores_test):
         """
         updates the class variable era_diff_test_total which holds
         the difference between the era scores of the test dataframe
@@ -124,7 +139,7 @@ class EraBooster:
         era_diff_test = pd.Series(index=test_df[self.date_col].unique())
         if self.iteration > 0:
             for era in era_scores_test.index:
-                era_diff_test[era] = era_scores_test[era] - era_scores_test_prior[era]
+                era_diff_test[era] = era_scores_test[era] - self.era_scores_test_prior[era]
                 # dic = {'iteration': 'it' + str(self.iteration), date_col: '_' + era[3:], 'correlation_changes': era_diff_test[era]}
                 dic = {
                     'iteration': f"it{str(self.iteration)}",
@@ -132,6 +147,8 @@ class EraBooster:
                     'correlation_changes': {era_diff_test[era]},
                 }
                 self.era_diff_test_total_lst.append(dic)
+        # keep present scores for future
+        self.era_scores_test_prior = era_scores_test.copy()
 
     def worst_scores(self, era_scores_train):
         """
